6d164d73000d2cec6e825ae3116c7090b4eade32
Jennabenni <je686804@ucf.edu>
2025-03-14 18:58:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ import App from './App';
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 registerRootComponent(App);
